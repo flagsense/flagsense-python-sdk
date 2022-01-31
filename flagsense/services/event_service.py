7b6e1fe40ea85531438e97be5ae4f15abe533a92
Flagsense <85953409+flagsense@users.noreply.github.com>
2022-01-31 19:35:58 +0530

    v1.0.7: Added method to record events for experiments (#1)

--- flagsense/services/event_service.py
+++ flagsense/services/event_service.py
@@ -11,20 +11,30 @@ from flagsense.util.utility import Utility
 
 class EventService:
 	def __init__(self, headers, environment):
+		machineId = str(uuid.uuid4())
 		self._data = {}
 		self._codeBugs = {}
 		self._errors = {}
+		self._experimentEvents = {}
 		self._requestBodyMap = {}
+		self._experimentEventsRequestBodyMap = {}
 		self._timeSlot = self._get_time_slot(time.time())
 		
 		self._headers = headers
 		self._body = {
-			'machineId': str(uuid.uuid4()),
+			'machineId': machineId,
 			'sdkType': 'python',
 			'environment': environment,
 			'data': None,
 			'codeBugs': None,
 			'errors': None,
+			'time': self._timeSlot
+		}
+		self._experimentEventsBody = {
+			'machineId': machineId,
+			'sdkType': 'python',
+			'environment': environment,
+			'experimentEvents': None,
 			'time': self._timeSlot
 		}
 		
@@ -75,6 +85,9 @@ class EventService:
 		try:
 			if not Constants.CAPTURE_EVENTS_FLAG:
 				return
+
+			if flagId == '' or variantKey == '':
+				return
 			
 			currentTimeSlot = self._get_time_slot(time.time())
 			if currentTimeSlot != self._timeSlot:
@@ -92,9 +105,48 @@ class EventService:
 		except Exception as err:
 			# print(err)
 			pass
+
+	def record_experiment_event(self, experimentId, eventName, variantKey, value):
+		try:
+			if not Constants.CAPTURE_EVENTS_FLAG:
+				return
+
+			currentTimeSlot = self._get_time_slot(time.time())
+			if currentTimeSlot != self._timeSlot:
+				self._check_and_refresh_data(currentTimeSlot)
+
+			metricsMap = {
+				'count': 1,
+				'total': value,
+				'minimum': value,
+				'maximum': value
+			}
+
+			if experimentId in self._experimentEvents:
+				if eventName in self._experimentEvents[experimentId]:
+					if variantKey in self._experimentEvents[experimentId][eventName]:
+						metricsMap = self._experimentEvents[experimentId][eventName][variantKey]
+						metricsMap['count'] = metricsMap['count'] + 1
+						metricsMap['total'] = metricsMap['total'] + value
+						metricsMap['minimum'] = min(metricsMap['minimum'], value)
+						metricsMap['maximum'] = max(metricsMap['maximum'], value)
+					self._experimentEvents[experimentId][eventName][variantKey] = metricsMap
+				else:
+					self._experimentEvents[experimentId][eventName] = {
+						variantKey: metricsMap
+					}
+			else:
+				self._experimentEvents[experimentId] = {
+					eventName: {
+						variantKey: metricsMap
+					}
+				}
+		except Exception as err:
+			# print(err)
+			pass
 	
 	def _get_time_slot(self, datetime):
-		return math.floor(datetime / Constants.EVENT_FLUSH_INTERVAL) * Constants.EVENT_FLUSH_INTERVAL * 1000
+		return math.ceil(datetime / Constants.EVENT_FLUSH_INTERVAL) * Constants.EVENT_FLUSH_INTERVAL * 1000
 	
 	def _register_shutdown_hook(self):
 		self._refresh_data(self._get_time_slot(time.time()))
@@ -106,17 +158,21 @@ class EventService:
 		self._refresh_data(currentTimeSlot)
 	
 	def _refresh_data(self, currentTimeSlot):
-		self._body['time'] = currentTimeSlot
+		self._body['time'] = self._timeSlot
 		self._body['data'] = self._data
 		self._body['codeBugs'] = self._codeBugs
 		self._body['errors'] = self._errors
-		
-		self._requestBodyMap[currentTimeSlot] = copy.deepcopy(self._body)
+		self._requestBodyMap[self._timeSlot] = copy.deepcopy(self._body)
+
+		self._experimentEventsBody['time'] = self._timeSlot
+		self._experimentEventsBody['experimentEvents'] = self._experimentEvents
+		self._experimentEventsRequestBodyMap[self._timeSlot] = copy.deepcopy(self._experimentEventsBody)
 		
 		self._timeSlot = currentTimeSlot
 		self._data = {}
 		self._codeBugs = {}
 		self._errors = {}
+		self._experimentEvents = {}
 	
 	def _start_event_sender(self):
 		self._polling_thread = threading.Thread(target=self._run)
@@ -150,13 +206,22 @@ class EventService:
 			if timeKey in self._requestBodyMap:
 				requestBody = self._requestBodyMap[timeKey]
 				if requestBody:
-					self._send_event(requestBody)
+					self._send_event('variantsData', requestBody)
 				del self._requestBodyMap[timeKey]
+
+		timeKeys = list(self._experimentEventsRequestBodyMap.keys())
+
+		for timeKey in timeKeys:
+			if timeKey in self._experimentEventsRequestBodyMap:
+				requestBody = self._experimentEventsRequestBodyMap[timeKey]
+				if requestBody:
+					self._send_event('experimentEvents', requestBody)
+				del self._experimentEventsRequestBodyMap[timeKey]
 	
-	def _send_event(self, requestBody):
+	def _send_event(self, api, requestBody):
 		try:
 			Utility.requests_retry_session().post(
-				Constants.EVENTS_BASE_URL + 'variantsData',
+				Constants.EVENTS_BASE_URL + api,
 				headers=self._headers,
 				json=requestBody,
 				timeout=10
